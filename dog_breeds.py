15c1d16e381b56e953741f4090c7da5ae0e265a4
Britton Jordan <brittonty@gmail.com>
2022-10-03 09:56:51 -0600

    test

--- dog_breeds.py
+++ dog_breeds.py
@@ -24,7 +24,7 @@ def download_imagaes_from_terms(terms: List, path_to_parent_dir: str, max_images
         resize_images(dest, max_size=400, dest=dest)
 
 if __name__ == '__main__':
-    PATH = Path("/Users/brittonjordan/Documents/personal-repos/dog-breeds/images")
+    PATH = Path("/Users/brittonjordan/Documents/personal-repos/dog-breeds")
 
     # download_imagaes_from_terms(searches, path, max_images=200)
 
@@ -38,13 +38,27 @@ if __name__ == '__main__':
         splitter=RandomSplitter(valid_pct=0.2, seed=42),
         get_y=parent_label,
         item_tfms=[Resize(192, method='squish')]
-    ).dataloaders(PATH)
+    ).dataloaders(f"{PATH}/images")
 
     dls.show_batch(max_n=6)
 
     learn = vision_learner(dls, resnet18, metrics=error_rate)
     learn.fine_tune(3)
 
-    category,_,probs = learn.predict(PILImage.create('/Users/brittonjordan/Documents/personal-repos/dog-breeds/Newfoundland1.jpeg'))
-    print(f"This is a: {category}.")
-    print(f"Probability it's a {category}: {probs[0]:.4f}")
+    breeds_to_predict = ["newfoundland", "chihuahua", "pug", "german_shepherd", "golden_retriever", "pomeranian", "saint_bernard", "siberian_husky", "yorkshire_terrier"]
+
+    for breed in breeds_to_predict:
+        try:
+            category, _, probs = learn.predict(PILImage.create(f"{PATH}/test_images/{breed}.jpeg"))
+            print(f"The image of breed {breed} was predicted to be a {category} with a probability of {probs[0]:.4f}")
+        except Exception as e:
+            print(f"Something went wrong with {breed}: {e}")
+
+    
+    # category,_,probs = learn.predict(PILImage.create('/Users/brittonjordan/Documents/personal-repos/dog-breeds/Newfoundland1.jpeg'))
+    # print(f"This is a: {category}.")
+    # print(f"Probability it's a {category}: {probs[0]:.4f}")
+
+    # category,_,probs = learn.predict(PILImage.create('/Users/brittonjordan/Documents/personal-repos/dog-breeds/chihuahua.jpeg'))
+    # print(f"This is a: {category}.")
+    # print(f"Probability it's a {category}: {probs[0]:.4f}")
